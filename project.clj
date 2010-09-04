5ed274be782585a30583ff004d0cf71f957f6a00
Rob Levy <rob@voronoi.(none)>
2010-09-04 13:35:00 -0400

    setting up basic files for project

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clojure-nltk "0.0.1"
+  :description "Python's Natural Language Toolkit for Clojure (interop / partial port)."
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [clojure-python "0.0.1"]]
+  :dev-dependencies [[lein-clojars "0.5.0"]])
---
+++ test/clojure_nltk/core_test.clj
@@ -0,0 +1,6 @@
+(ns clojure-nltk.core-test
+  (:use [clojure-nltk.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
