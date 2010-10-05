fedbcf8292003866de600bd4148821fb1f6c90b8
Rob Levy <rob@voronoi.(none)>
2010-10-05 03:36:00 -0400

    added macro for setting up nltk

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clojure-nltk "0.0.3"
+(defproject clojure-nltk "0.0.4"
   :description "Python's Natural Language Toolkit for Clojure (interop / partial port)."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [clojure-python "0.0.2"]]
+                 [clojure-python "0.0.3"]]
   :dev-dependencies [[lein-clojars "0.5.0"]]
   :main clojure-nltk.core)
--- src/clojure_nltk/core.clj
+++ src/clojure_nltk/core.clj
@@ -2,11 +2,24 @@
   (:use clojure-nltk.utils)
   (:require clojure-python.core))
 
-(defn init
-  "load nltk"
-  []
-  (clojure-python.core/init (get-nltk-path))
-  (clojure-python.core/py-import nltk))
+(defmacro nltk-init
+  "set up ntlk. currently supported usages:
+  (nltk-init (:import foo bar baz))"
+  [& clauses]
+  (let [import-clauses (set (apply concat (map #(if (= :import (first %)) (rest %)) clauses)))
+        ;; possibly define other types of clauses here
+        ]
+    `(do
+       (clojure-python.core/init
+        (clojure-nltk.utils/get-nltk-path))
+       (clojure-python.core/py-import ~'nltk)
+       ~@(map (fn [module]
+                `(clojure-python.core/py-import
+                  ~'nltk
+                  ~module))
+              import-clauses))))
 
+(defn lazy-corpus [pyobj]
+  (clojure-python.core/pyobj-iterate pyobj))
 
-;; TODO: add public API to interact with NLTK functions
+;; TODO: add to this public API into NLTK functions
